109362d9a3ea00b68a34529112c70d6378dfae8c
Anna Clara Luna <annaclara.luna06@gmail.com>
2025-09-15 17:54:30 -0300

    Cataloguei os produtos do jardim

---
+++ view/grid-pagina-produtos.js
@@ -0,0 +1,40 @@
+fetch('prod.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Erro ao carregar o JSON");
+    }
+    return response.json();
+  })
+  .then(data => {
+    const produtos = data.produtos;
+    const filtrados = produtos.filter(p => p.id >= 11 && p.id <= 23);
+
+    filtrados.forEach(p => {
+      const card = document.createElement('div');
+      card.className = 'card';
+
+      const img = document.createElement('img');
+      img.src = p.imagem;
+      img.alt = p.nome;
+
+      const info = document.createElement('div');
+      info.className = 'card-info';
+      info.textContent = `${p.nome} - (R$ ${p.preco})`;
+
+      const button = document.createElement('button');
+      button.className = 'btn-carrinho';
+      button.textContent = 'Adicionar ao carrinho';
+
+      button.addEventListener('click', () => {
+        adicionarAoCarrinho(p.nome, p.id, p.preco, p.imagem);
+      });
+
+      card.appendChild(img);
+      card.appendChild(info);
+      grid.appendChild(card);
+      card.appendChild(button);
+    });
+  })
+  .catch(error => {
+    console.error("Erro:", error);
+  });
